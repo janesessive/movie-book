ce243f28350b414bf8bb80d34e2c2cb2d84c09a3
janesessive <eugene.absessiveabs@gmail.com>
2019-10-01 12:30:00 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -3,31 +3,14 @@ import './App.css';
 import Movies from './components/Movies/Movies';
 import Genres from './components/Genres/Genres';
 import Actors from './components/Actors/Actors';
+import Navbar from './components/Navbar/Navbar';
 import { Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">
-              Movies
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/genres">
-              Genres
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/actors">
-              Actors
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div>
+      <Navbar/>
 
       <main className="container">
 
@@ -38,7 +21,7 @@ class App extends Component {
           
         </Switch>
       </main>
-    </React.Fragment>
+    </div>
   );
 }
 }
