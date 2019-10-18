edad2498b68e439f86d16b423bc36fb9d8c46ee4
janesessive <eugene.absessiveabs@gmail.com>
2019-10-18 15:16:41 +0300

    Genres table added

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 import "./Genres.css";
 import GenreForm from "../GenreForm/GenreForm";
 import  * as dataService  from '../../dataService';
+import DataTable from '../../DataTable/DataTable';
 
 class Genres extends Component {
-  componentDidMount = async() => {
+  state = {
+    genres: [],
+    isEditMode: false,
+    currentGenre: {
+      _id: null,
+      name: ''
+    }
+  };
+
+  loadGenres=async()=>{
     const genres = await dataService.getGenres();
     this.setState({ genres });
- };
+  }
+
+  componentDidMount = () => {
+    this.loadGenres();
+   };
   // async componentDidMount() {
   //   try {
   //     let response = await fetch("http://localhost:4000/api/genres");
@@ -24,10 +38,46 @@ class Genres extends Component {
   //   }
    
   // }
+  onButtonClickMultiDelete = () => {
+    const genres = [...this.state.genres];
+    const updatedGenres = genres.filter(categ => categ.isSelected !== true);
+    this.setState({ genres: updatedGenres });
+  };
+  onSelectedRecord = (genre, e) => {
+    const index = this.state.genres.findIndex(e => e._id === genre._id);
+    if (index !== -1) {
+      const genres = [...this.state.genres];
+      genres[index] = { ...genre, isSelected: e.target.checked };
+      this.setState({ genres });
+    }
+  };
+  onClickEditHandler = genre => {
+    this.setState({
+      currentGenre: {
+        ...genre //do clone itself
+      },
+      isEditMode: true
+    });
+  };
+  deleteRecordHandler = async genreId => {
+    await dataService.deleteGenre(genreId);
+    await this.loadGenres();
+    
+  };
   render() {
     return (
       <div>
         <GenreForm />
+        <DataTable
+              columns={[
+                { header: 'Genre', index: 'name' },
+              ]}
+              records={this.state.genres}
+              onMultipleDelete={this.onButtonClickMultiDelete}
+              onRecordSelected={this.onSelectedRecord}          
+              onRecordEdit={this.onClickEditHandler}
+              onRecordDelete={this.deleteRecordHandler}
+            />
         <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
