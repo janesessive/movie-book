89379011beb3adb14851ef31e57aa14fb75ce647
janesessive <eugene.absessiveabs@gmail.com>
2019-10-21 12:37:22 +0300

    ActorsSelectForm

---
+++ src/components/ActorsSelectForm/ActorsSelectForm.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+
+const ActorsSelectForm = props => {
+    let [actors, setActors] = useState({
+      _id: null,
+      firstName: "",
+      lastName: "",
+      gender: ""
+    });
+
+    return (
+        <div>
+            </div>
+  );
+};
+
+export default ActorsSelectForm;
